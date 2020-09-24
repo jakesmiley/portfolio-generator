edd7e7cc97e7853e26b571a157464e1a3470056c
Jake Smiley <jake.alan.smiley@gmail.com>
2020-09-24 12:57:06 -0600

    pushing for review

--- app.js
+++ app.js
@@ -68,15 +68,15 @@ Add a New Project
             message: 'Would you like to enter another project?',
             default: false
         }
-        // .then(projectData => {
-        //     portfolioData.projects.push(projectData);
-        //     if (projectData.confirmAddProject) {
-        //       return promptProject(portfolioData);
-        //     } else {
-        //       return portfolioData;
-        //     }
-        //   });
-    ]);
+    ])
+    // .then(projectData => {
+    //     portfolioData.projects.push(projectData);
+    //     if (projectData.feature) {
+    //       return promptProject(portfolioData);
+    //     } else {
+    //       return portfolioData;
+    //     }
+    //   });
 };
 
 promptUser()
